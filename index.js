50082cd98d6368a5c51500bff6c9db2c2188762e
Christopher Cantu <chrischimi02@gmail.com>
2024-03-14 12:20:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function returnCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
